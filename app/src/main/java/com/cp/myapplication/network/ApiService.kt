b4e3052402b5f99a2f86ce86f67abcd90a39d074
Allesson Silva <126684613+AlleSilvaa@users.noreply.github.com>
2024-11-04 13:44:48 -0300

    Update ApiService.kt
    Adicionando métodos do CRUD na interface.

--- app/src/main/java/com/cp/myapplication/network/ApiService.kt
+++ app/src/main/java/com/cp/myapplication/network/ApiService.kt
@@ -1,9 +1,18 @@
 package com.cp.myapplication.network
 
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface ApiService {
     @GET("itinerarios")
     fun getItinerarios(): Call<List<Itinerario>>
+
+    @POST("itinerarios")
+    fun createItinerario(@Body itinerario: Itinerario): Call<Itinerario>
+
+    @PUT("itinerarios/{id}")
+    fun updateItinerario(@Path("id") id: String, @Body itinerario: Itinerario): Call<Itinerario>
+
+    @DELETE("itinerarios/{id}")
+    fun deleteItinerario(@Path("id") id: String): Call<Void>
 }
