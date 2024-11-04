c5e19b4cd8f62484cc072e91b59ca44d180629f6
Allesson Silva <126684613+AlleSilvaa@users.noreply.github.com>
2024-11-04 14:21:42 -0300

    Update RetrofitClient.kt

--- app/src/main/java/com/cp/myapplication/network/RetrofitClient.kt
+++ app/src/main/java/com/cp/myapplication/network/RetrofitClient.kt
@@ -1,16 +1,15 @@
-package com.cp.planejeja.network
+package com.cp.myapplication.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://api.example.com/" // Altere para a URL da sua API
+    private const val BASE_URL = "https://sua_api_url/" // Substitua pela URL da sua API
 
-    val apiService: ApiService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiService::class.java)
-    }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
 }
